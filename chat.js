81275acf1faed5701f9638f84cd7a3155e7cc6c1
Don Martial <rokibyusuf7@gmail.com>
2026-01-23 09:13:30 +0100

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,10 +1,12 @@
-// 🔥 REPLACE WITH YOUR FIREBASE CONFIG
 const firebaseConfig = {
   apiKey: "AIzaSyBjYUgidUaapX8PXmdBa06du74mj0Y9Vcw",
   authDomain: "mychatapp-d3e7a.firebaseapp.com",
   projectId: "mychatapp-d3e7a",
+  storageBucket: "mychatapp-d3e7a.firebasestorage.app",
+  messagingSenderId: "580728953641",
   appId: "1:580728953641:web:9f068f0163fd9113737538"
 };
+
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
