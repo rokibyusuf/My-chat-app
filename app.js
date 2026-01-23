13a487940ac64c8aff64ca5834dfbcd72477ebf6
Don Martial <rokibyusuf7@gmail.com>
2026-01-23 09:08:19 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
-// 🔥 REPLACE WITH YOUR FIREBASE CONFIG
 const firebaseConfig = {
   apiKey: "AIzaSyBjYUgidUaapX8PXmdBa06du74mj0Y9Vcw",
   authDomain: "mychatapp-d3e7a.firebaseapp.com",
   projectId: "mychatapp-d3e7a",
+  storageBucket: "mychatapp-d3e7a.firebasestorage.app",
+  messagingSenderId: "580728953641",
   appId: "1:580728953641:web:9f068f0163fd9113737538"
 };
 
-firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
 const auth = firebase.auth();
 
-function loginWithGoogle() {
+function googleLogin() {
   const provider = new firebase.auth.GoogleAuthProvider();
-  const msg = document.getElementById("message");
 
   auth.signInWithPopup(provider)
     .then(() => {
-      msg.innerText = "Login successful 🎉";
-      msg.style.color = "green";
-      setTimeout(() => {
-        window.location.href = "username.html";
-      }, 300);
+      window.location.href = "username.html";
     })
-    .catch(err => {
-      msg.innerText = err.message;
-      msg.style.color = "red";
-    });
+    .catch(err => alert(err.message));
 }
